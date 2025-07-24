c3bbd64bf18d0b5c9ca02044cc711bf3f90a3827
Ashok <ashoklord@gmail.com>
2025-07-25 01:27:56 +0530

    API methods added

--- src/test/java/utilities/APIUtils.java
+++ src/test/java/utilities/APIUtils.java
@@ -1,4 +1,37 @@
 package utilities;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
 public class APIUtils {
+
+    public Response response = null;
+
+    public void sendAPIRequest(String requestType, String endPoint, String payload) {
+        try {
+            RestAssured.baseURI = "";
+            switch (requestType) {
+                case "get":
+                    response = given().
+                            when().get(endPoint).
+                            then().extract().response();
+                    break;
+                case "post":
+                    response = given()
+                            .when().body(payload)
+                            .post(endPoint).then().extract().response();
+                    break;
+                case "put":
+                    response = given()
+                            .when().body(payload)
+                            .put(endPoint).then().extract().response();
+                    break;
+            }
+        }catch(Exception  e) {
+            e.printStackTrace();
+        }
+    }
+
 }
